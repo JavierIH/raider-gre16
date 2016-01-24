c8c42b9a1ef6489cf547e6482709f9c887348d59
JavierIH <javier.i.h@hotmail.com>
2016-01-24 10:21:45 +0000

    zero function

--- dynamixel.py
+++ dynamixel.py
@@ -1,5 +1,5 @@
 import serial
-import time
+
 
 class Dynamixel(object):
 
@@ -7,22 +7,21 @@ class Dynamixel(object):
         self.com = serial.Serial(port, baud_rate)
 
     def setPosition(self, id, position):
-        self.com.write(self._coder(1,id,30,position))
+        self.com.write(self._coder(1, id, 30, position))
 
     def setSpeed(self, id, speed):
-        self.com.write(self._coder(1,id,32,speed))
+        self.com.write(self._coder(1, id, 32, speed))
 
     def getPosition(self, id):
-	self.com.write(self._coder(0,id,36,0))
-	low=com.read()
-        self.com.write(self._coder(0,id,37,0))
-        high=com.read()
-        return (ord(high)<<8) + ord(low)
+        self.com.write(self._coder(0, id, 36, 0))
+        low = self.com.read()
+        self.com.write(self._coder(0, id, 37, 0))
+        high = self.com.read()
+        return (ord(high) << 8) + ord(low)
 
     def _coder(self, rw, id, reg, val):
         command = 'M'
-        command += chr((rw<<7) + id)
-        command += chr((reg<<2) + (val>>8))
+        command += chr((rw << 7) + id)
+        command += chr((reg << 2) + (val >> 8))
         command += chr(val % 256)
         return command
-
--- raider.py
+++ raider.py
@@ -10,13 +10,47 @@ import dynamixel
 
 class Raider(object):
 
-    def __init__(self, servo_trims, servo_ids, name='raider'):
+    def __init__(self, servo_trims, name='raider'):
 
         # Configuration
         self._name = name
         self._servo_trims = servo_trims
-        self._servo_ids = servo_ids
         self.dxl = dynamixel.Dynamixel()
+
+    def move(self, id, position):
+        self.dxl.com.write(self.dxl._coder(1, id, 30, position))
+        if id == 17 or id == 18:
+            self.dxl.com.write(self.dxl._coder(1, id+100, 30, position))
+
+    def zero(self):
+        for i in range(0,11):
+            self.move(i, 512)
+        for i in range(13,25):
+            self.move(i, 512)
+
+    def home(self, h=0):
+        self.move(1, 512)
+        self.move(2, 512)
+        self.move(3, 512)
+        self.move(4, 512)
+        self.move(5, 512)
+        self.move(6, 512)
+        self.move(7, 512)
+        self.move(8, 512)
+        self.move(9, 512)
+        self.move(10, 512)
+        self.move(13, 512)
+        self.move(14, 512)
+        self.move(15, 512)
+        self.move(16, 512)
+        self.move(17, 512-h)
+        self.move(18, 512+h)
+        self.move(19, 512+h)
+        self.move(20, 512-h)
+        self.move(21, 512+h)
+        self.move(22, 512-h)
+        self.move(23, 512)
+        self.move(24, 512)
 
 
     def fake(self, steps, T=750.0):
@@ -44,7 +78,3 @@ class Raider(object):
             osc.refresh()
             self.dxl.setPosition(6,int(osc.output))
             self.dxl.setPosition(4,int(osc.output))
-
-
-robot = Raider(0,0)
-robot.fake(3)
